d5c7a48ff005ce6bc0dd229a69ff428bbb78a910
Ang <angelinalan@student.unimelb.edu.au>
2026-06-06 21:21:58 +1000

    added const for products

---
+++ products.js
@@ -0,0 +1,17 @@
+const products = [
+    { 
+        id: 1,
+        title: "RADIANT DRESS",
+        price: "$50",
+        description: "Dress",
+        colors: [
+            {
+                name: "Red",
+                mainImage: "assets\images\productsPage\radiantDressThumbnail.png",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    }
+]
