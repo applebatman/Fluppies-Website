83bffb3142eab49f25f28ede93d6106be00995f8
Ang <angelinalan@student.unimelb.edu.au>
2026-06-08 16:21:13 +1000

    adding products detail page

--- products.js
+++ products.js
@@ -7,7 +7,52 @@ const products = [
         colors: [
             {
                 name: "Red",
-                mainImage: "assets\images\productsPage\radiantDressThumbnail.png",
+                mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+        { 
+        id: 2,
+        title: "RADIANT DRESS",
+        price: "$50",
+        description: "Dress",
+        colors: [
+            {
+                name: "Red",
+                mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+        { 
+        id: 3,
+        title: "RADIANT DRESS",
+        price: "$50",
+        description: "Dress",
+        colors: [
+            {
+                name: "Red",
+                mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+        { 
+        id: 4,
+        title: "RADIANT DRESS",
+        price: "$50",
+        description: "Dress",
+        colors: [
+            {
+                name: "Red",
+                mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
                 thumbnails: [],
                 sizes: ["S", "M", "L", "XL", "XXL"]
             }
--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-1 //html different ID
-
+//initialiseCarasol();
 const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null;
 
-//initialiseCarasol();
+//only runs if slide images exist on page
+if (slides.length>0) {
 document.addEventListener("DOMContentLoaded", initialiseCarasol)
+
+}
 
 function initialiseCarasol() {
     if(slides.length > 0) {
@@ -18,6 +20,7 @@ function initialiseCarasol() {
 
 
 function showSlide(index){
+    if(slides.length===0) return;
 
     if(index >= slides.length){
         slideIndex = 0;
@@ -43,4 +46,33 @@ function nextSlide(){
     showSlide(slideIndex);
 }
 
+//display products
+const productContainer = document.querySelector(".product-list");
+
+if (productContainer) {
+    displayProducts();
+}
+
+function displayProducts() {
+    products.forEach(product=> {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        productCard.innerHTML = `
+        <div class="img-box">
+            <img src="${product.colors[0].mainImage}">
+        </div>
+        <h2 class="title">${product.title}</h2>
+        <span class = "price">${product.price}</span>
+        `;
+        productContainer.appendChild(productCard);
+        
+        const imgBox = productCard.querySelector(".img-box");
+        imgBox.addEventListener("click",() => {
+            sessionStorage.setItem("selectedProduct", JSON.stringify(product));
+            window.location.href = "product-detail.html";
+        })
+    }
+
+    )
+}
 
