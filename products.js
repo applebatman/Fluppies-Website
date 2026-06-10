76eb309f52940f0dc0f42525dd4f5a3c511a50d6
Ang <angelinalan@student.unimelb.edu.au>
2026-06-10 11:50:38 +1000

    adding more products

--- products.js
+++ products.js
@@ -4,10 +4,10 @@ const products = [
         title: "RADIANT DRESS",
         category:"Dresses",
         price: "$50",
-        description: "Dress",
+        description: "The perfect polka dot dress for your dog",
         colors: [
             {
-                name: "Red",
+                name: "Polka Dot",
                 mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
                 thumbnails: [
                     "assets/images/productsPage/radiantDress3.png",
@@ -27,10 +27,10 @@ const products = [
         title: "FRILLY DRESS",
         category:"Dresses",
         price: "$50",
-        description: "Blue dress made of ",
+        description: "Pink tulle dress for all the aspiring dog ballerinas out there",
         colors: [
             {
-                name: "Red",
+                name: "Pink",
                 mainImage: "assets/images/productsPage/frillyDress.png",
                 thumbnails: [],
                 sizes: ["S", "M", "L", "XL", "XXL"]
@@ -40,14 +40,14 @@ const products = [
     },
         { 
         id: 3,
-        title: "RADIANT DRESS",
-        category:"Collars",
-        price: "$50",
-        description: "Dress",
+        title:"STAR DRESS",
+        category:"Dresses",
+        price: "$20",
+        description: "The perfect dress made out of stars, dedicated to our perfect furry star friends!",
         colors: [
             {
-                name: "Red",
-                mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
+                name: "Blue",
+                mainImage: "assets/images/productsPage/starDress.png",
                 thumbnails: [],
                 sizes: ["S", "M", "L", "XL", "XXL"]
             }
@@ -56,14 +56,14 @@ const products = [
     },
         { 
         id: 4,
-        title: "RADIANT DRESS",
-        category:"Collars",
+        title: "DENIM HAT",
+        category:"Hats",
         price: "$50",
-        description: "Dress",
+        description: "Smooth to feel, good for blocking sun out of eyes and looks super chic",
         colors: [
             {
-                name: "Red",
-                mainImage: "assets/images/productsPage/radiantDressThumbnail.png",
+                name: "Blue",
+                mainImage: "assets/images/productsPage/denimHat.png",
                 thumbnails: [],
                 sizes: ["S", "M", "L", "XL", "XXL"]
             }
@@ -71,7 +71,91 @@ const products = [
 
     },
 
+            { 
+        id: 5,
+        title: "GENTE HAT",
+        category:"Hats",
+        price: "$20",
+        description: "The perfect hat for a gentleman",
+        colors: [
+            {
+                name: "Blue",
+                mainImage: "assets/images/productsPage/genteHat.png",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
 
+    },
+
+            { 
+        id: 6,
+        title: "COWBOY HAT",
+        category:"Hats",
+        price: "$50",
+        description: "YEEHAWW",
+        colors: [
+            {
+                name: "Blue",
+                mainImage: "assets/images/productsPage/cowboyHat.png",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+            { 
+        id: 7,
+        title: "BLACK COLLAR",
+        category:"Collars",
+        price: "$10",
+        description: "YEEHAWW",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "assets/images/productsPage/collarTree1.jpg",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+
+     { 
+        id: 8,
+        title: "BROWN COLLAR",
+        category:"Collars",
+        price: "$10",
+        description: "YEEHAWW",
+        colors: [
+            {
+                name: "Blue",
+                mainImage: "assets/images/productsPage/collarTree2.jpg",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+
+     { 
+        id: 9,
+        title: "FISH COLLAR",
+        category:"Collars",
+        price: "$10",
+        description: "YEEHAWW",
+        colors: [
+            {
+                name: "Blue",
+                mainImage: "assets/images/productsPage/collarTree3.jpg",
+                thumbnails: [],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+
+    },
+
+    
 
     
 
